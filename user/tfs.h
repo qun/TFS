c16e96ea284af2bf602630915f5d8b12dbc0d922
Guanqun Lu <guanqun.lu.list@gmail.com>
2009-07-05 17:22:43 +0800

    add TFS_* prefix to macros.
    Signed-off-by: Guanqun Lu <guanqun.lu.list@gmail.com>

--- user/tfs.h
+++ user/tfs.h
@@ -3,17 +3,17 @@
 
 #define TFS_MAGIC 0x544653;
 #define TFS_VERSION 0x1
-#define BLOCK_SIZE 1024
-#define MAX_INODE_BLOCKS 1024
-#define MAX_DATA_BLOCKS (1024 * 100)
-#define MAX_BLOCKS_PER_FILE 100
+#define TFS_BLOCK_SIZE 1024
+#define TFS_MAX_INODE_BLOCKS 1024
+#define TFS_MAX_DATA_BLOCKS (1024 * 100)
+#define TFS_MAX_BLOCKS_PER_FILE 100
 
 struct tfs_sb {
 	int s_magic;
 	int s_version;
-	int s_inode[MAX_INODE_BLOCKS];
+	int s_inode[TFS_MAX_INODE_BLOCKS];
 	int s_inode_nr;
-	int s_data[MAX_DATA_BLOCKS];
+	int s_data[TFS_MAX_DATA_BLOCKS];
 	int s_data_nr;
 };
 
@@ -26,7 +26,7 @@ struct tfs_inode {
 	int i_mode;
 	int i_links;
 	int i_size;
-	int i_blocks[MAX_BLOCKS_PER_FILE];
+	int i_blocks[TFS_MAX_BLOCKS_PER_FILE];
 	int i_blocks_nr;
 };
 
