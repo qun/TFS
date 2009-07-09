7598afc8698e66279054b3bae28eb8831245dfd3
Guanqun Lu <guanqun.lu.list@gmail.com>
2009-07-10 07:21:25 +0800

    fix 'not a directory' error.
    This commit fixes the 'not a directory' error
    when I try to change to the mounted directory.
    It's a stub actually.
    
    Signed-off-by: Guanqun Lu <guanqun.lu.list@gmail.com>

--- kern/dir.c
+++ kern/dir.c
@@ -6,8 +6,15 @@ static int tfs_create(struct inode *dir, struct dentry *dentry,
 	return 0;
 }
 
+struct dentry *tfs_lookup(struct inode *inode, struct dentry *de, struct nameidata *nd)
+{
+	printk("tfs_lookup invoked!\n");
+	return NULL;
+}
+
 const struct inode_operations tfs_dir_inops = {
 	.create		= tfs_create,
+	.lookup		= tfs_lookup,
 };
 
 const struct file_operations tfs_dir_operations = {
